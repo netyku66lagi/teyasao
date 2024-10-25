f25a54fd7c9a001b6ec229912fdca245994263ba
netyku66lagi <netylagi003@hotmail.com>
2024-10-25 14:17:19 +0700

    Create node.sh

---
+++ node.sh
@@ -0,0 +1,3 @@
+curl -X POST https://api.vercel.com/v1/integrations/deploy/prj_z49QlQuntXHrx48yVX53sLluIvOo/ynpKjnTFXV
+curl -X POST https://api.vercel.com/v1/integrations/deploy/prj_4PWMNK3tHzOrmTo8yFXILzU8rpb9/vkajZqX7Ov
+curl -X POST https://api.vercel.com/v1/integrations/deploy/prj_ZBPKTkD7OY4dWT6kz3nB8KgnRkQE/cP5PpcGcTb
